98380a0732f3d8605f46996a439b869358b3c9c1
Yosuf Gurung <yosufgurung@gmail.com>
2018-12-18 07:53:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 function destructivelyAppendKitten(kittens) {
-  kittens.pop("Ralph"
+  kittens.pop("Ralph")
   return kittens
 }
 
